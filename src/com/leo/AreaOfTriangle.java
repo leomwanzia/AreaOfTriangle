f36ab470e6ed3ae1ae833558eee79e859acb289f
leo <leomutungi19@gmail.com>
2024-09-27 19:17:08 +0300

    Area of triangle

--- src/com/leo/AreaOfTriangle.java
+++ src/com/leo/AreaOfTriangle.java
@@ -2,8 +2,22 @@ package com.leo;
 
 import java.util.Scanner;
 
-public class AreaOfTriangle {
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+public class AreaOfTriangle { //public class that should match the file name
+    public static void main(String[] args) { //where the execution of the program starts
+        Scanner in = new Scanner(System.in); //reads from the standard input of the user
+
+        System.out.println("Enter the base of the triangle :"); //prompt to the user
+        // reading the double input of the base
+        double base = in.nextDouble();
+
+        System.out.println("Enter the height of the triangle :"); // prompt to enter the height of the triangle
+        // reading the double input for the height
+        double height = in.nextDouble();
+
+        // Area = 0.5 x base x height
+        double area = 0.5 * base * height;
+        System.out.println("The area of the triangle is :" + area);
+
+
     }
 }
